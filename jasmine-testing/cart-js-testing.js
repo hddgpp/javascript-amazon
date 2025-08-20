9d3384437ba95a231efc19e48a43ed85972458bd
hddgpp <youssefhehe3@gmail.com>
2025-08-20 14:01:03 +0100

    start using the jasming testing frame work to test the cart localStorage

---
+++ jasmine-testing/cart-js-testing.js
@@ -0,0 +1,33 @@
+import {getCart, saveCart} from '../data/cart.js'
+
+
+describe("Cart functions", () => {
+  beforeEach(() => {
+    // Clear localStorage before each test
+    localStorage.clear();
+  });
+
+  it("should return an empty array if no cart is saved", () => {
+    expect(getCart()).toEqual([]);
+  });
+
+  it("should save and retrieve the cart correctly", () => {
+    const fakeCart = [{ id: 1, quantity: 2 }];
+    saveCart(fakeCart);
+
+    const result = getCart();
+    expect(result).toEqual(fakeCart);
+  });
+
+  it("should overwrite old cart when saving new one", () => {
+    const cart1 = [{ id: 1, quantity: 2 }];
+    const cart2 = [{ id: 99, quantity: 1 }];
+
+    saveCart(cart1);
+    saveCart(cart2);
+
+    const result = getCart();
+    expect(result).toEqual(cart2);
+  });
+});
+
